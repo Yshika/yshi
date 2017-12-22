e2f9d084aeb0994d2a95c4a4e3798632ea71e4be
harsh kumar saxena <harshsaxena163@gmail.com>
2017-12-22 21:27:06 +0530

    program on operations

---
+++ y.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+voidmain()
+{
+int a[4][3],b[4][3],c[4][3],i,t;
+printf ("enter 20 erray);
+for(j=0,J<3,J++)
+{
+scanf("%d"&a[i][j]);
+}
+}
+printf("enter the second'b'");
+{
+for(j=0,j<3,j++);
+{
+scanf("%d"&b[i][j]);
+}
+c[i][j]=a[i][j]+b[i][j];
+}
+for(i=0;i<4;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("%d",c[i][t]);
+}
+}
